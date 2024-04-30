2f57387948dd1f22240748abd2dacf9124f2945d
Grace <graceodonnell2013@gmail.com>
2024-04-30 18:27:02 +0100

    Randomise color on hover and change opacity

--- backend.js
+++ backend.js
@@ -2,9 +2,13 @@ let num = 16;
 
 function changeGridSize(num) {
   num = Number(window.prompt("Type a number from 1 to 99", ""));
-  gridContainer.replaceChildren();
-  console.log(num);
-  createGrid(num);
+  if (num > 100) {
+    Number(window.prompt("Please type a number from 1 to 99", ""));
+  } else {
+    gridContainer.replaceChildren();
+    console.log(num);
+    createGrid(num);
+  }
 }
 
 // Get the grid container element
@@ -23,9 +27,21 @@ function createGrid(num) {
       rowDiv.append(cell);
 
       cell.addEventListener("mouseover", (event) => {
-        cell.style.backgroundColor = "lightblue";
+        cell.style.backgroundColor = generateRandomColor();
+        console.log(cell.style.opacity);
+        let cellOpacity = Number(cell.style.opacity);
+        cell.style.opacity = 0.1 + cellOpacity;
       });
     }
   }
 }
 createGrid(num);
+
+function generateRandomColor() {
+  let maxVal = 0xffffff; // 16777215
+  let randomNumber = Math.random() * maxVal;
+  randomNumber = Math.floor(randomNumber);
+  randomNumber = randomNumber.toString(16);
+  let randColor = randomNumber.padStart(6, 0);
+  return `#${randColor.toUpperCase()}`;
+}
