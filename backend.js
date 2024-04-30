6074364de0c4e9227e7229164c06c99d00221e37
Grace <graceodonnell2013@gmail.com>
2024-04-30 12:39:58 +0100

    Create grid using flexbox

--- backend.js
+++ backend.js
@@ -1,17 +1,20 @@
-const rows = 16;
-const columns = 16;
+const rows = 6;
+const columns = 6;
 
 // Get the grid container element
 const gridContainer = document.querySelector(".grid-container");
 
 // Create the grid dynamically
 for (let row = 0; row < rows; row++) {
+  const rowDiv = document.createElement("div");
+  rowDiv.className = "row";
+  gridContainer.append(rowDiv);
   for (let column = 0; column < columns; column++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    cell.innerText = `${row}, ${column}`;
+    rowDiv.append(cell);
     console.log(row);
-    console.log(column);
-    const gridItem = document.createElement("div");
-    gridItem.className = "grid-item";
-
-    gridContainer.appendChild(gridItem);
+    console.log(cell);
   }
 }
