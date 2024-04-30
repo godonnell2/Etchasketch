544727746c6bec8b2954e68b44792da37164f9ac
Grace <graceodonnell2013@gmail.com>
2024-04-30 17:12:11 +0100

    Create button that changes grid size

--- backend.js
+++ backend.js
@@ -1,22 +1,31 @@
-const rows = 16;
-const columns = 16;
+let num = 16;
+
+function changeGridSize(num) {
+  num = Number(window.prompt("Type a number from 1 to 99", ""));
+  gridContainer.replaceChildren();
+  console.log(num);
+  createGrid(num);
+}
 
 // Get the grid container element
 const gridContainer = document.querySelector(".grid-container");
 
-// Create the grid dynamically
-for (let row = 0; row < rows; row++) {
-  const rowDiv = document.createElement("div");
-  rowDiv.className = "row";
-  gridContainer.append(rowDiv);
-  for (let column = 0; column < columns; column++) {
-    const cell = document.createElement("div");
-    cell.className = "cell";
-    //cell.innerText = `${row}, ${column}`;
-    rowDiv.append(cell);
+function createGrid(num) {
+  // Create the grid dynamically
+  for (let row = 0; row < num; row++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.className = "row";
+    gridContainer.append(rowDiv);
+    for (let column = 0; column < num; column++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      //cell.innerText = `${row}, ${column}`;
+      rowDiv.append(cell);
 
-    cell.addEventListener("mouseover", (event) => {
-      cell.style.backgroundColor = "lightblue";
-    });
+      cell.addEventListener("mouseover", (event) => {
+        cell.style.backgroundColor = "lightblue";
+      });
+    }
   }
 }
+createGrid(num);
